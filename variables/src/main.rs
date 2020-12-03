1217c6a59d5d9db6b9ce87653a040047527c4ec6
burnsn3 <burnsn3@wwu.edu>
2020-12-02 22:57:17 -0800

    Completed variables section

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    //constants must have a datatype.
+    //const MAX_POINTS: u32 = 100000;
+
+    //this process is called "shadowing" it allows us to "change" immutable variables.
+    //x is currently 5
+    let x = 5;
+    //x is then 5 + 1
+    let x = x + 1;
+    //x is finally 6 * 2
+    let x = x * 2;
+    //if we used mut, the variable would be able to be changed after these operations
+    //since we shadowed it, the variable is immutable after these operations.
+    println!("The value of x is: {}", x);
+    
 }
